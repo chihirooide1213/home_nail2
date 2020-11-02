c12b13450303258083fe02acbab988704bfaa276
chihirooide1213 <donnakachi@gmail.com>
2020-11-02 06:22:46 +0000

    add_search

--- app/controllers/nail_comments_controller.rb
+++ app/controllers/nail_comments_controller.rb
@@ -1,10 +1,25 @@
 class NailCommentsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_comment, only: [:show, :edit, :update, :destroy]
   def index
+  	@nail_commnets = Nail_comennt.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  def destroy
+  end
+
+  private
+  def nail_comment_params
+  	params.require(:nail_comment).permit(:user_id, :nail_id, :rate, :content, :title)
+  end
+
+  def set_comment
+  	@nail = Nail.find(params[:id])
   end
 end
