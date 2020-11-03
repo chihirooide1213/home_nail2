702b8294c97b9666dcdce02dc9f518afc33f40b5
chihirooide1213 <donnakachi@gmail.com>
2020-11-03 06:02:43 +0000

    ranking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+	if params[:name].present?
+      @search_items = Nail.where('name Like ?', "%#{params[:name]}%")
+    else
+      @search_items = Nail.none
+    end
 end
