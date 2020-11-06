a0ff0e1154519eeb03a842bba12f708e0df0d037
chihirooide1213 <donnakachi@gmail.com>
2020-11-06 11:22:18 +0000

    search_box

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-	if params[:name].present?
-      @search_items = Nail.where('name Like ?', "%#{params[:name]}%")
-    else
-      @search_items = Nail.none
-    end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
+
   def top
   	now = Time.current
-  	monthly_ranking = Nail_comment.where(created_at: time.now.all_month).group(:nail_id).order("avg(nail_comments.rate) desc").limit(3).pluck("nail_id,avg(nail_comments.rate)")
+  	monthly_ranking = NailComment.where(created_at: Time.now.all_month).group(:nail_id).order("avg(nail_comments.rate) desc").limit(3).pluck("nail_id,avg(nail_comments.rate)")
   	@monthly_ranking = monthly_ranking.map do |r|
   		{
   			nail:Nail.find(r[0]),
@@ -9,7 +10,7 @@ class HomeController < ApplicationController
   		}
   	end
 
-  	weekly_ranking = Nail_comment.where(created_at: time.now.all_week).group(:nail_id).order("avg(nail_comments.rate) desc").limit(3).pluck("nail_id,avg(nail_comments.rate)")
+  	weekly_ranking = NailComment.where(created_at: Time.now.all_week).group(:nail_id).order("avg(nail_comments.rate) desc").limit(3).pluck("nail_id,avg(nail_comments.rate)")
   	@weekly_ranking = weekly_ranking.map do |r|
   		{
   			nail:Nail.find(r[0]),
@@ -17,12 +18,18 @@ class HomeController < ApplicationController
   		}
   	end
 
-  	daily_ranking = Nail_comment.where(created_at: time.now.all_daily).group(:nail_id).order("avg(nail_comments.rate) desc").limit(3).pluck("nail_id,avg(nail_comments.rate)")
+  	daily_ranking = NailComment.where(created_at: Time.now.all_day).group(:nail_id).order("avg(nail_comments.rate) desc").limit(3).pluck("nail_id,avg(nail_comments.rate)")
   	@daily_ranking = daily_ranking.map do |r|
   		{
   			nail:Nail.find(r[0]),
   			average:r[1]
   		}
   	end
+
+  	if params[:name].present?
+      @search_nails = Nail.where('name Like ?', "%#{params[:name]}%")
+    else
+      @search_nails = Nail.none
+    end
   end
 end
